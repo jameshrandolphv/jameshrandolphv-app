54ae0d3cc3fb37d89ae066b5874c8a186c128d14
jameshrandolphv <jameshrandolphv@gmail.com>
2018-08-04 19:43:17 -0400

    Added a few neat things :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jameshrandolphv-app';
+
+  batteryLevel = '100%';
+
+  constructor() {
+    navigator.getBattery().then(
+      (battery) => {
+        this.batteryLevel = battery.level * 100 + "%";
+        battery.addEventListener('levelchange', () => this.batteryLevel = battery.level * 100 + "%");
+      }
+    );
+  }
+
+  today = new Date();
+
+  getTime() {
+    let h = this.today.getHours() + '';
+    let m = this.today.getMinutes() < 10 ? '0' + this.today.getMinutes() : this.today.getMinutes() + '';
+    return h + ':' + m;
+  }
 }
