29b47aa762848834c0eb66c45dd5cf774852aa23
jameshrandolphv <jameshrandolphv@gmail.com>
2018-08-04 22:43:39 -0400

    Added dock.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatDividerModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatDividerModule, MatIconModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
@@ -9,13 +9,15 @@ import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { BioCardComponent } from './home/bio-card/bio-card.component';
 import { MusicCardComponent } from './home/music-card/music-card.component';
+import { DockComponent } from './dock/dock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BioCardComponent,
     HomeComponent,
-    MusicCardComponent
+    MusicCardComponent,
+    DockComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,8 @@ import { MusicCardComponent } from './home/music-card/music-card.component';
     AppRoutingModule,
     MatCardModule,
     FlexLayoutModule,
-    MatDividerModule
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
